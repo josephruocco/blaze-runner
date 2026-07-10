e62ba41fe44a015100a99ec1325163fadbc1547b
josephruocco <ruoccoj19@gmail.com>
2026-07-10 14:27:27 -0400

    Add reproducible itch.io build script

---
+++ build-itch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Build the self-contained itch.io zip from the current working tree.
+# Run this at each milestone, then upload blaze-runner-itch.zip to itch.io.
+set -e
+cd "$(dirname "$0")"
+rm -rf blaze-runner-deploy blaze-runner-itch.zip
+mkdir blaze-runner-deploy
+cp index.html game.js phaser.min.js cover.png blaze-runner-deploy/
+( cd blaze-runner-deploy && zip -q -r ../blaze-runner-itch.zip . )
+echo "Built blaze-runner-itch.zip:"
+unzip -l blaze-runner-itch.zip
